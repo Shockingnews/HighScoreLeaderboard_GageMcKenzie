267d16ba7d9b32b3f2072fd8d2ffc139ab8074f1
Shockingnews <gagemckenzie70@gmail.com>
2025-10-29 15:19:08 -0300

    points

--- HighScoreLeaderboard_GageMcKenzie/Program.cs
+++ HighScoreLeaderboard_GageMcKenzie/Program.cs
@@ -4,14 +4,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Threading;
+using System.Diagnostics;
 
 namespace HighScoreLeaderboard_GageMcKenzie
 {
     internal class Program
     {
+        static Random score = new Random();
         static void Main(string[] args)
         {
+            // string[] Inputs = { };
+            Console.Clear();
+            string userInput = "";
+            int yourscore = score.Next(1, 1001);
+            string path = @"data.txt";
+            
+            string data = File.ReadAllText(path);
 
+            
+
+            Console.WriteLine("write three initials: ");
+
+            userInput = Console.ReadLine();
+            Console.WriteLine("Your score is: " + yourscore);
+            data += (yourscore + " " + userInput + ',');
+
+            string[] test = data.Split(',');
+
+            File.WriteAllText(path, data);
+            Array.Sort(test);
+            Array.Reverse(test);
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.WriteLine(test[i]);
+            }
+
+            
+
+
+
+            //Console.WriteLine(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine(Inputs[0]);
+
+            //if (userInput.Length != 3)
+            //{
+                //Console.WriteLine("There isn't three initials try again");
+            //}
+
+            // Process notepad = Process.Start("notepad.exe", path);
+
+
+            // for (int i = 0; i < data.Length; i++)
+            // {
+                // Console.WriteLine(data[i]);
+            // }
+            
+            
+            // Thread.Sleep(1000);
+            
         }
     }
 }
